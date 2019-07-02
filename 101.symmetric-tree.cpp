cc9465eb0b22fbcbd5605e50146e6c73a1585bbd
RingInFour <15347109301@163.com>
2019-07-02 18:41:20 +0800

    update 101.symmetric-tree.cpp

---
+++ 101.symmetric-tree.cpp
@@ -0,0 +1,70 @@
+/*
+ * @lc app=leetcode id=101 lang=cpp
+ *
+ * [101] Symmetric Tree
+ *
+ * https://leetcode.com/problems/symmetric-tree/description/
+ *
+ * algorithms
+ * Easy (43.49%)
+ * Total Accepted:    417.2K
+ * Total Submissions: 954.7K
+ * Testcase Example:  '[1,2,2,3,4,4,3]'
+ *
+ * Given a binary tree, check whether it is a mirror of itself (ie, symmetric
+ * around its center).
+ * 
+ * For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+ * 
+ * 
+ * ⁠   1
+ * ⁠  / \
+ * ⁠ 2   2
+ * ⁠/ \ / \
+ * 3  4 4  3
+ * 
+ * 
+ * 
+ * 
+ * But the following [1,2,2,null,3,null,3] is not:
+ * 
+ * 
+ * ⁠   1
+ * ⁠  / \
+ * ⁠ 2   2
+ * ⁠  \   \
+ * ⁠  3    3
+ * 
+ * 
+ * 
+ * 
+ * Note:
+ * Bonus points if you could solve it both recursively and iteratively.
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        if (root == NULL) return true;
+        return compareRL(root->left, root->right);
+    }
+    //left and right means the node's position relative to the root
+    bool compareRL(TreeNode *left, TreeNode *right) {
+        if (!left && !right) return true;
+        if (!left || !right) return false;
+        if (left->val != right->val) return false;
+
+        return compareRL(left->left, right->right) &&
+                compareRL(left->right, right->left);
+        
+    }
+};
