34c4d04f798649d77455de6b0cfc4025e4accb10
RingInFour <15347109301@163.com>
2019-07-11 18:04:54 +0800

    update 69.sqrtx.cpp

--- 69.sqrtx.cpp
+++ 69.sqrtx.cpp
@@ -46,13 +46,15 @@ public:
         int low = 1, high = x / 2;
         while (low < high) {
             long long mid = (low + high) / 2;
-            if (mid * mid > x)
+            long long lower = mid * mid;
+            long long upper = (mid + 1) * (mid + 1);
+            if (lower > x)
                 high = mid;
-            else if (mid * mid < x)
-                low = mid + 1;
-            else if (mid * mid == x) 
+            else if (lower <= x && upper > x)
                 return mid;
+            else
+                low = mid + 1;
         }
-        return high - 1;
+        return 0;
     }
 };
