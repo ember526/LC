a238ba7370671a6b814f6cb16cf0c0e17a90ff39
RingInFour <15347109301@163.com>
2019-07-20 18:28:02 +0800

    update 44.wildcard-matching.cpp

---
+++ 44.wildcard-matching.cpp
@@ -0,0 +1,114 @@
+/*
+ * @lc app=leetcode id=44 lang=cpp
+ *
+ * [44] Wildcard Matching
+ *
+ * https://leetcode.com/problems/wildcard-matching/description/
+ *
+ * algorithms
+ * Hard (23.03%)
+ * Total Accepted:    184.5K
+ * Total Submissions: 799.5K
+ * Testcase Example:  '"aa"\n"a"'
+ *
+ * Given an input string (s) and a pattern (p), implement wildcard pattern
+ * matching with support for '?' and '*'.
+ * 
+ * 
+ * '?' Matches any single character.
+ * '*' Matches any sequence of characters (including the empty sequence).
+ * 
+ * 
+ * The matching should cover the entire input string (not partial).
+ * 
+ * Note:
+ * 
+ * 
+ * s could be empty and contains only lowercase letters a-z.
+ * p could be empty and contains only lowercase letters a-z, and characters
+ * like ? or *.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input:
+ * s = "aa"
+ * p = "a"
+ * Output: false
+ * Explanation: "a" does not match the entire string "aa".
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input:
+ * s = "aa"
+ * p = "*"
+ * Output: true
+ * Explanation: '*' matches any sequence.
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input:
+ * s = "cb"
+ * p = "?a"
+ * Output: false
+ * Explanation: '?' matches 'c', but the second letter is 'a', which does not
+ * match 'b'.
+ * 
+ * 
+ * Example 4:
+ * 
+ * 
+ * Input:
+ * s = "adceb"
+ * p = "*a*b"
+ * Output: true
+ * Explanation: The first '*' matches the empty sequence, while the second '*'
+ * matches the substring "dce".
+ * 
+ * 
+ * Example 5:
+ * 
+ * 
+ * Input:
+ * s = "acdcb"
+ * p = "a*c?b"
+ * Output: false
+ * 
+ * 
+ */
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int si = 0, pi = 0, aster = -1, aster_match = 0;
+        while (si < s.length()) {
+            if (pi < p.length() && (p[pi] == s[si] || p[pi] == '?')) {
+                ++si;
+                ++pi;
+            }
+            
+            else if (pi < p.length() && p[pi] == '*') {
+                aster = pi;
+                aster_match = si;
+                ++pi;
+            }
+            
+            else if (aster > -1) {
+                si = ++aster_match;
+                pi = aster + 1;
+            }
+
+            else 
+                return false;
+        }
+        
+        while (pi < p.length())
+            if (p[pi++] != '*')
+                return false;
+        return true;
+    }
+};
