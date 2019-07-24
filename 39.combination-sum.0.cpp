8cc91c92e1094ef327799f2a4964aaadb87bdd8c
RingInFour <15347109301@163.com>
2019-07-24 20:56:22 +0800

    update 39.combination-sum.0.cpp

---
+++ 39.combination-sum.0.cpp
@@ -0,0 +1,79 @@
+/*
+ * @lc app=leetcode id=39 lang=cpp
+ *
+ * [39] Combination Sum
+ *
+ * https://leetcode.com/problems/combination-sum/description/
+ *
+ * algorithms
+ * Medium (49.33%)
+ * Total Accepted:    366.8K
+ * Total Submissions: 739.6K
+ * Testcase Example:  '[2,3,6,7]\n7'
+ *
+ * Given a set of candidate numbers (candidates) (without duplicates) and a
+ * target number (target), find all unique combinations in candidates where the
+ * candidate numbers sums to target.
+ * 
+ * The same repeated number may be chosen from candidates unlimited number of
+ * times.
+ * 
+ * Note:
+ * 
+ * 
+ * All numbers (including target) will be positive integers.
+ * The solution set must not contain duplicate combinations.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: candidates = [2,3,6,7], target = 7,
+ * A solution set is:
+ * [
+ * ⁠ [7],
+ * ⁠ [2,2,3]
+ * ]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: candidates = [2,3,5], target = 8,
+ * A solution set is:
+ * [
+ * [2,2,2,2],
+ * [2,3,3],
+ * [3,5]
+ * ]
+ * 
+ * 
+ */
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int> cur;
+        vector<vector<int>> result;
+        dfs(candidates, target, 0, 0, cur, result);
+        return result;
+    }
+private:
+    void dfs(const vector<int> nums,
+             int target,
+             int lastElem, int curSum, vector<int> &cur, vector<vector<int>>& result) {
+        if ( curSum > target)
+            return;
+        if (curSum == target) {
+            result.push_back(cur);
+            return;
+        }
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] < lastElem)
+                continue;
+            cur.push_back(nums[i]);
+            dfs(nums, target, nums[i], curSum + nums[i], cur, result);
+            cur.pop_back();
+        }
+        return;
+    }
+};
