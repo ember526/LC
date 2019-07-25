7be230ec0c57985fc28f228bc37186d7694d8947
RingInFour <15347109301@163.com>
2019-07-26 00:27:49 +0800

    update 46.permutations.cpp

---
+++ 46.permutations.cpp
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode id=46 lang=cpp
+ *
+ * [46] Permutations
+ *
+ * https://leetcode.com/problems/permutations/description/
+ *
+ * algorithms
+ * Medium (55.29%)
+ * Total Accepted:    393.5K
+ * Total Submissions: 705.3K
+ * Testcase Example:  '[1,2,3]'
+ *
+ * Given a collection of distinct integers, return all possible permutations.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: [1,2,3]
+ * Output:
+ * [
+ * ⁠ [1,2,3],
+ * ⁠ [1,3,2],
+ * ⁠ [2,1,3],
+ * ⁠ [2,3,1],
+ * ⁠ [3,1,2],
+ * ⁠ [3,2,1]
+ * ]
+ * 
+ * 
+ */
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        if (nums.empty())
+            return {};
+        vector<vector<int>> result;
+        function<void(vector<int>, int)> recur = [&](vector<int> nums, int swapi) {
+            if (swapi == nums.size() - 1) {
+                result.push_back(nums);
+                return;
+            }
+            for (int i = swapi; i < nums.size(); ++i) {
+                swap(nums[swapi], nums[i]);
+                recur(nums, swapi + 1);
+            }
+        };
+        recur(nums, 0);
+        return result;
+    }
+};
