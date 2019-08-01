ef7dd617833e2fe534ac61142f0f9058e44115de
RingInFour <15347109301@163.com>
2019-08-01 17:54:18 +0800

    update 583.delete-operation-for-two-strings.cpp

---
+++ 583.delete-operation-for-two-strings.cpp
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode id=583 lang=cpp
+ *
+ * [583] Delete Operation for Two Strings
+ *
+ * https://leetcode.com/problems/delete-operation-for-two-strings/description/
+ *
+ * algorithms
+ * Medium (45.20%)
+ * Total Accepted:    33.7K
+ * Total Submissions: 74.5K
+ * Testcase Example:  '"sea"\n"eat"'
+ *
+ * 
+ * Given two words word1 and word2, find the minimum number of steps required
+ * to make word1 and word2 the same, where in each step you can delete one
+ * character in either string.
+ * 
+ * 
+ * Example 1:
+ * 
+ * Input: "sea", "eat"
+ * Output: 2
+ * Explanation: You need one step to make "sea" to "ea" and another step to
+ * make "eat" to "ea".
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * The length of given words won't exceed 500.
+ * Characters in given words can only be lower-case letters.
+ * 
+ * 
+ */
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        vector<vector<int>> dp(word1.length() + 1, vector<int>(word2.length() + 1, 0));
+        for (int i = 1; i <= word1.length(); ++i)
+            for (int j = 1; j <= word2.length(); ++j) {
+                if (word1[i-1] == word2[j-1])
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                dp[i][j] = max(dp[i][j], max(dp[i-1][j], dp[i][j-1]));
+            }
+        return word1.length() + word2.length() - 2 * dp[word1.length()][word2.length()];
+    }
+};
