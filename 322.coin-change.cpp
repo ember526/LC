0afe8dde033cce404853393afbd39256a967530e
RingInFour <15347109301@163.com>
2019-08-01 18:47:40 +0800

    update 322.coin-change.cpp

---
+++ 322.coin-change.cpp
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode id=322 lang=cpp
+ *
+ * [322] Coin Change
+ *
+ * https://leetcode.com/problems/coin-change/description/
+ *
+ * algorithms
+ * Medium (30.95%)
+ * Total Accepted:    213.6K
+ * Total Submissions: 688.2K
+ * Testcase Example:  '[1,2,5]\n11'
+ *
+ * You are given coins of different denominations and a total amount of money
+ * amount. Write a function to compute the fewest number of coins that you need
+ * to make up that amount. If that amount of money cannot be made up by any
+ * combination of the coins, return -1.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: coins = [1, 2, 5], amount = 11
+ * Output: 3 
+ * Explanation: 11 = 5 + 5 + 1
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: coins = [2], amount = 3
+ * Output: -1
+ * 
+ * 
+ * Note:
+ * You may assume that you have an infinite number of each kind of coin.
+ * 
+ */
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount + 1, -5);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; ++i) {
+            for (int j = coins.size() - 1; j >= 0; --j) {
+                if (i - coins[j] < 0 || dp[i - coins[j]] < 0)
+                    continue;
+                dp[i] = dp[i] < 0 ? dp[i - coins[j]] + 1 : min(dp[i], dp[i - coins[j]] + 1);
+            }
+        }
+        return dp[amount] < 0 ? -1 : dp[amount];
+    }
+};
