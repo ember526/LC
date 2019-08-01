139f237f3075e8317e4cb201106e98cbe29b5a01
RingInFour <15347109301@163.com>
2019-08-01 17:56:19 +0800

    update 583.delete-operation-for-two-strings.cpp

--- 583.delete-operation-for-two-strings.cpp
+++ 583.delete-operation-for-two-strings.cpp
@@ -41,7 +41,8 @@ public:
             for (int j = 1; j <= word2.length(); ++j) {
                 if (word1[i-1] == word2[j-1])
                     dp[i][j] = dp[i-1][j-1] + 1;
-                dp[i][j] = max(dp[i][j], max(dp[i-1][j], dp[i][j-1]));
+                else
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
             }
         return word1.length() + word2.length() - 2 * dp[word1.length()][word2.length()];
     }
