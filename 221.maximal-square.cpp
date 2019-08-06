ddff6578027da95d8dfee847aac4c1d39354f031
RingInFour <15347109301@163.com>
2019-08-06 14:16:17 +0800

    update 221.maximal-square.cpp

---
+++ 221.maximal-square.cpp
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode id=221 lang=cpp
+ *
+ * [221] Maximal Square
+ *
+ * https://leetcode.com/problems/maximal-square/description/
+ *
+ * algorithms
+ * Medium (33.39%)
+ * Total Accepted:    141K
+ * Total Submissions: 418.9K
+ * Testcase Example:  '[["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]'
+ *
+ * Given a 2D binary matrix filled with 0's and 1's, find the largest square
+ * containing only 1's and return its area.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: 
+ * 
+ * 1 0 1 0 0
+ * 1 0 1 1 1
+ * 1 1 1 1 1
+ * 1 0 0 1 0
+ * 
+ * Output: 4
+ * 
+ */
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        if (matrix.empty() || matrix[0].empty())        
+            return 0;
+        const int m = matrix.size(), n = matrix[0].size();
+        int maxlen = 0;
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+        for (int i = 1; i <= m; ++i)
+            for (int j = 1; j <= n; ++j)
+                if (matrix[i-1][j-1] == '1') {
+                    dp[i][j] = 1 + min(dp[i-1][j-1], min(dp[i-1][j], dp[i][j-1]));
+                    maxlen = max(maxlen, dp[i][j]);
+                }
+        return maxlen * maxlen;
+    }
+};
