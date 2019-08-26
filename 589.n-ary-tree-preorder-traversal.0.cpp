6152153978e95ded420d0fc04591acf593c11c8b
RingInFour <15347109301@163.com>
2019-08-26 11:20:59 -0500

    update 589.n-ary-tree-preorder-traversal.0.cpp

---
+++ 589.n-ary-tree-preorder-traversal.0.cpp
@@ -0,0 +1,65 @@
+/*
+ * @lc app=leetcode id=589 lang=cpp
+ *
+ * [589] N-ary Tree Preorder Traversal
+ *
+ * https://leetcode.com/problems/n-ary-tree-preorder-traversal/description/
+ *
+ * algorithms
+ * Easy (68.51%)
+ * Total Accepted:    54.8K
+ * Total Submissions: 79.9K
+ * Testcase Example:  '{"$id":"1","children":[{"$id":"2","children":[{"$id":"5","children":[],"val":5},{"$id":"6","children":[],"val":6}],"val":3},{"$id":"3","children":[],"val":2},{"$id":"4","children":[],"val":4}],"val":1}'
+ *
+ * Given an n-ary tree, return the preorder traversal of its nodes' values.
+ * 
+ * For example, given a 3-ary tree:
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * Return its preorder traversal as: [1,3,5,6,2,4].
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * Recursive solution is trivial, could you do it iteratively?
+ * 
+ */
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+class Solution {
+public:
+    vector<int> preorder(Node* root) {
+        vector<int> result;
+        stack<Node *> st;
+        if (root == NULL)
+            return result;
+        st.push(root);
+        while (st.empty() == false) {
+            Node *node = st.top();
+            st.pop();
+            result.push_back(node->val);
+            for (auto i = rbegin(node->children); i != rend(node->children); ++i)
+                st.push(*i);
+        }
+        return result;
+    }
+};
