ab76a87835f9b1df506661108393db520c80325b
RingInFour <15347109301@163.com>
2019-08-25 23:01:02 -0500

    update 94.binary-tree-inorder-traversal.cpp

---
+++ 94.binary-tree-inorder-traversal.cpp
@@ -0,0 +1,56 @@
+/*
+ * @lc app=leetcode id=94 lang=cpp
+ *
+ * [94] Binary Tree Inorder Traversal
+ *
+ * https://leetcode.com/problems/binary-tree-inorder-traversal/description/
+ *
+ * algorithms
+ * Medium (57.19%)
+ * Total Accepted:    482.4K
+ * Total Submissions: 842.3K
+ * Testcase Example:  '[1,null,2,3]'
+ *
+ * Given a binary tree, return the inorder traversal of its nodes' values.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: [1,null,2,3]
+ * ⁠  1
+ * ⁠   \
+ * ⁠    2
+ * ⁠   /
+ * ⁠  3
+ * 
+ * Output: [1,3,2]
+ * 
+ * Follow up: Recursive solution is trivial, could you do it iteratively?
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> result;
+        inorder(root, result);
+        return result;
+    }
+private:
+    void inorder(TreeNode *node, vector<int> &result) {
+        if (node == NULL)
+            return;
+        inorder(node->left, result);
+        result.push_back(node->val);
+        inorder(node->right, result);
+        return;
+    }
+};
