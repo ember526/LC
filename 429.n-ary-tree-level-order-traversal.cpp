c7eb05bf59651920c8add2ff9e1ebe7299b88ff9
RingInFour <15347109301@163.com>
2019-08-28 11:27:20 -0500

    update 429.n-ary-tree-level-order-traversal.cpp

---
+++ 429.n-ary-tree-level-order-traversal.cpp
@@ -0,0 +1,81 @@
+/*
+ * @lc app=leetcode id=429 lang=cpp
+ *
+ * [429] N-ary Tree Level Order Traversal
+ *
+ * https://leetcode.com/problems/n-ary-tree-level-order-traversal/description/
+ *
+ * algorithms
+ * Easy (60.35%)
+ * Total Accepted:    42.8K
+ * Total Submissions: 70.7K
+ * Testcase Example:  '{"$id":"1","children":[{"$id":"2","children":[{"$id":"5","children":[],"val":5},{"$id":"6","children":[],"val":6}],"val":3},{"$id":"3","children":[],"val":2},{"$id":"4","children":[],"val":4}],"val":1}'
+ *
+ * Given an n-ary tree, return the level order traversal of its nodes' values.
+ * (ie, from left to right, level by level).
+ * 
+ * For example, given a 3-ary tree:
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * We should return its level order traversal:
+ * 
+ * 
+ * [
+ * ⁠    [1],
+ * ⁠    [3,2,4],
+ * ⁠    [5,6]
+ * ]
+ * 
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * The depth of the tree is at most 1000.
+ * The total number of nodes is at most 5000.
+ * 
+ * 
+ */
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+class Solution {
+public:
+    vector<vector<int>> levelOrder(Node* root) {
+        vector<vector<int>> result;
+        vector<Node *> layer;
+        if (root == NULL)
+            return result;
+        layer.push_back(root);
+        while (layer.empty() == false) {
+            vector<Node *> next;
+            vector<int> tmp;
+            for (Node *node : layer) {
+                tmp.push_back(node->val);
+                for (Node *child : node->children)
+                    next.push_back(child);
+            }
+            layer.swap(next);
+            result.push_back(tmp);
+        }
+        return result;
+    }
+};
