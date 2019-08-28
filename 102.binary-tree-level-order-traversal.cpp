f861969d44c24be8710a64e7cf9df56758764278
RingInFour <15347109301@163.com>
2019-08-27 21:16:00 -0500

    update 102.binary-tree-level-order-traversal.cpp

---
+++ 102.binary-tree-level-order-traversal.cpp
@@ -0,0 +1,72 @@
+/*
+ * @lc app=leetcode id=102 lang=cpp
+ *
+ * [102] Binary Tree Level Order Traversal
+ *
+ * https://leetcode.com/problems/binary-tree-level-order-traversal/description/
+ *
+ * algorithms
+ * Medium (48.56%)
+ * Total Accepted:    392.1K
+ * Total Submissions: 800.4K
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * Given a binary tree, return the level order traversal of its nodes' values.
+ * (ie, from left to right, level by level).
+ * 
+ * 
+ * For example:
+ * Given binary tree [3,9,20,null,null,15,7],
+ * 
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ * 
+ * 
+ * 
+ * return its level order traversal as:
+ * 
+ * [
+ * ⁠ [3],
+ * ⁠ [9,20],
+ * ⁠ [15,7]
+ * ]
+ * 
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> result;
+        if (root == NULL)
+            return result;
+        vector<TreeNode *> layer;
+        layer.push_back({root});
+        int level = 0;
+        while (!layer.empty()) {
+            vector<int> tmp;
+            vector<TreeNode *>next;
+            for (auto node : layer) {
+                tmp.push_back(node->val);
+                if (node->left)
+                    next.push_back(node->left);
+                if (node->right)
+                    next.push_back(node->right);
+            }
+            result.push_back(tmp);
+            layer.swap(next);
+        }
+        return result;
+    }
+};
