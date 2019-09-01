ef15f4c3dc560cc325a9d3d21f0fe7213e800b14
RingInFour <15347109301@163.com>
2019-09-01 09:54:13 -0500

    update 530.minimum-absolute-difference-in-bst.cpp

---
+++ 530.minimum-absolute-difference-in-bst.cpp
@@ -0,0 +1,76 @@
+/*
+ * @lc app=leetcode id=530 lang=cpp
+ *
+ * [530] Minimum Absolute Difference in BST
+ *
+ * https://leetcode.com/problems/minimum-absolute-difference-in-bst/description/
+ *
+ * algorithms
+ * Easy (51.22%)
+ * Total Accepted:    66K
+ * Total Submissions: 128.6K
+ * Testcase Example:  '[1,null,3,2]'
+ *
+ * Given a binary search tree with non-negative values, find the minimum
+ * absolute difference between values of any two nodes.
+ * 
+ * Example:
+ * 
+ * 
+ * Input:
+ * 
+ * ⁠  1
+ * ⁠   \
+ * ⁠    3
+ * ⁠   /
+ * ⁠  2
+ * 
+ * Output:
+ * 1
+ * 
+ * Explanation:
+ * The minimum absolute difference is 1, which is the difference between 2 and
+ * 1 (or between 2 and 3).
+ * 
+ * 
+ * 
+ * 
+ * Note: There are at least two nodes in this BST.
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int getMinimumDifference(TreeNode* root) {
+        stack<TreeNode *> s;
+        auto cur = root;
+        int mindis = INT_MAX;
+        bool set = false;
+        int last = 0;
+        while (cur || !s.empty()) {
+            while (cur) {
+                s.push(cur);
+                cur = cur->left;
+            }
+            cur = s.top();
+            s.pop();
+            if (set) {
+                mindis = min(mindis, cur->val - last);
+                last = cur->val;
+            }
+            else {
+                set = true;
+                last = cur->val;
+            }
+            cur = cur->right;
+        }
+        return mindis;
+    }
+};
