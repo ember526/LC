f3ac1e115fed44aefad1be54729f53200c696bf0
RingInFour <15347109301@163.com>
2019-09-02 10:23:08 -0500

    update 501.find-mode-in-binary-search-tree.cpp

---
+++ 501.find-mode-in-binary-search-tree.cpp
@@ -0,0 +1,91 @@
+/*
+ * @lc app=leetcode id=501 lang=cpp
+ *
+ * [501] Find Mode in Binary Search Tree
+ *
+ * https://leetcode.com/problems/find-mode-in-binary-search-tree/description/
+ *
+ * algorithms
+ * Easy (39.96%)
+ * Total Accepted:    62.4K
+ * Total Submissions: 155.9K
+ * Testcase Example:  '[1,null,2,2]'
+ *
+ * Given a binary search tree (BST) with duplicates, find all the mode(s) (the
+ * most frequently occurred element) in the given BST.
+ * 
+ * Assume a BST is defined as follows:
+ * 
+ * 
+ * The left subtree of a node contains only nodes with keys less than or equal
+ * to the node's key.
+ * The right subtree of a node contains only nodes with keys greater than or
+ * equal to the node's key.
+ * Both the left and right subtrees must also be binary search trees.
+ * 
+ * 
+ * 
+ * 
+ * For example:
+ * Given BST [1,null,2,2],
+ * 
+ * 
+ * ⁠  1
+ * ⁠   \
+ * ⁠    2
+ * ⁠   /
+ * ⁠  2
+ * 
+ * 
+ * 
+ * 
+ * return [2].
+ * 
+ * Note: If a tree has more than one mode, you can return them in any order.
+ * 
+ * Follow up: Could you do that without using any extra space? (Assume that the
+ * implicit stack space incurred due to recursion does not count).
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> findMode(TreeNode* root) {
+        if (root == NULL)
+            return {};
+        unordered_map<int, int> m;
+        stack<TreeNode *> s;
+        TreeNode *pred = NULL;
+        auto cur = root;
+        while (cur || !s.empty()) {
+            while (cur) {
+                s.push(cur);
+                cur = cur->left;
+            }
+            cur = s.top();
+            s.pop();
+            ++m[cur->val];
+            pred = cur;
+            cur = cur->right;
+        }
+        if (m.empty())
+            return {root->val};
+        int modenum = 0;
+        for (auto &p : m)
+            if (p.second > modenum)
+                modenum = p.second;
+        vector<int> result;
+        for (auto &p : m)
+            if (p.second == modenum)
+                result.push_back(p.first);
+        return result;
+    }
+};
