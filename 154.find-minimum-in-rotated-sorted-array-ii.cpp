666746229ad8466220805d4ccaed76131b398510
RingInFour <15347109301@163.com>
2019-09-05 21:51:55 -0500

    update 154.find-minimum-in-rotated-sorted-array-ii.cpp

---
+++ 154.find-minimum-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,58 @@
+/*
+ * @lc app=leetcode id=154 lang=cpp
+ *
+ * [154] Find Minimum in Rotated Sorted Array II
+ *
+ * https://leetcode.com/problems/find-minimum-in-rotated-sorted-array-ii/description/
+ *
+ * algorithms
+ * Hard (39.67%)
+ * Total Accepted:    141.5K
+ * Total Submissions: 356K
+ * Testcase Example:  '[1,3,5]'
+ *
+ * Suppose an array sorted in ascending order is rotated at some pivot unknown
+ * to you beforehand.
+ * 
+ * (i.e.,  [0,1,2,4,5,6,7] might become  [4,5,6,7,0,1,2]).
+ * 
+ * Find the minimum element.
+ * 
+ * The array may contain duplicates.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [1,3,5]
+ * Output: 1
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [2,2,2,0,1]
+ * Output: 0
+ * 
+ * Note:
+ * 
+ * 
+ * This is a follow up problem to Find Minimum in Rotated Sorted Array.
+ * Would allow duplicates affect the run-time complexity? How and why?
+ * 
+ * 
+ */
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int l = 0, r = nums.size() - 1;
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] == nums[r])
+                --r;
+            else if (nums[mid] < nums[r])
+                r = mid;
+            else
+                l = mid + 1;
+        }
+        return nums[l];
+    }
+};
