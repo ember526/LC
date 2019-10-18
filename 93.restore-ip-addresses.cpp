3bec28db336a542fc417c3d9b01b16e2512bbae6
RingInFour <15347109301@163.com>
2019-10-18 10:57:09 -0500

    update 93.restore-ip-addresses.cpp

--- 93.restore-ip-addresses.cpp
+++ 93.restore-ip-addresses.cpp
@@ -6,9 +6,11 @@
  * https://leetcode.com/problems/restore-ip-addresses/description/
  *
  * algorithms
- * Medium (31.83%)
- * Total Accepted:    147.8K
- * Total Submissions: 460.3K
+ * Medium (32.45%)
+ * Likes:    824
+ * Dislikes: 351
+ * Total Accepted:    155.9K
+ * Total Submissions: 477.1K
  * Testcase Example:  '"25525511135"'
  *
  * Given a string containing only digits, restore it by returning all possible
@@ -22,33 +24,39 @@
  * 
  * 
  */
+
+// @lc code=start
 class Solution {
 public:
     vector<string> restoreIpAddresses(string s) {
-        if (s.length() < 4)
-            return {};
         vector<string> result;
-        partition(s, 3, 0, 1, result);
+        partition(s, 0, result, 4);
         return result;
     }
 private:
-    void partition(string s, int k, int start, int len, vector<string> &result) {
-        if (k == 0) {
-            string remain = s.substr(start);
-            if (remain.length() < 4 && stoi(remain) <= 255 && (remain.length() == 1 || remain[0] != '0'))
+    inline bool validSub(const string &s, int start, int length) {
+        string sub = s.substr(start, length);
+        if (stoi(sub) > 255 || (length > 1 && s[start] == '0'))
+            return false;
+        return true;
+    }
+
+    void partition(string s, int pos, vector<string> &result, int k) {
+        if (s.length() - pos > k * 3 || s.length() - pos < k)
+            return;
+        if (k == 1) {
+           if (validSub(s, pos, s.length() - pos))
                 result.push_back(s);
             return;
         }
-        for (int i = start + len - 1; i < s.length() - k; ++i) {
-            const string sub = s.substr(start, i - start + 1);
-            if (stoi(sub) > 255)
+        for (int i = pos; i < s.length(); ++i) {
+            if (!validSub(s, pos, i - pos + 1))
                 break;
             string tmp = s;
-            tmp.insert(i + 1, ".");
-            partition(tmp, k - 1, i + 2, 1, result);
-            if (s[start] == '0')
-                break;
+            tmp.insert(i + 1, 1, '.');
+            partition(tmp, i + 2, result, k - 1);
         }
         return;
     }
 };
+// @lc code=end
