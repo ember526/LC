566335bb6aca987eebbffb43364b8baf60f6a23d
RingInFour <15347109301@163.com>
2019-10-29 23:11:50 -0500

    update 824.goat-latin.cpp

---
+++ 824.goat-latin.cpp
@@ -0,0 +1,94 @@
+/*
+ * @lc app=leetcode id=824 lang=cpp
+ *
+ * [824] Goat Latin
+ *
+ * https://leetcode.com/problems/goat-latin/description/
+ *
+ * algorithms
+ * Easy (58.58%)
+ * Likes:    186
+ * Dislikes: 468
+ * Total Accepted:    41.2K
+ * Total Submissions: 69.9K
+ * Testcase Example:  '"I speak Goat Latin"'
+ *
+ * A sentence S is given, composed of words separated by spaces. Each word
+ * consists of lowercase and uppercase letters only.
+ * 
+ * We would like to convert the sentence to "Goat Latin" (a made-up language
+ * similar to Pig Latin.)
+ * 
+ * The rules of Goat Latin are as follows:
+ * 
+ * 
+ * If a word begins with a vowel (a, e, i, o, or u), append "ma" to the end of
+ * the word.
+ * For example, the word 'apple' becomes 'applema'.
+ * 
+ * If a word begins with a consonant (i.e. not a vowel), remove the first
+ * letter and append it to the end, then add "ma".
+ * For example, the word "goat" becomes "oatgma".
+ * 
+ * Add one letter 'a' to the end of each word per its word index in the
+ * sentence, starting with 1.
+ * For example, the first word gets "a" added to the end, the second word gets
+ * "aa" added to the end and so on.
+ * 
+ * 
+ * Return the final sentence representing the conversion from S to Goat
+ * Latin. 
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: "I speak Goat Latin"
+ * Output: "Imaa peaksmaaa oatGmaaaa atinLmaaaaa"
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: "The quick brown fox jumped over the lazy dog"
+ * Output: "heTmaa uickqmaaa rownbmaaaa oxfmaaaaa umpedjmaaaaaa overmaaaaaaa
+ * hetmaaaaaaaa azylmaaaaaaaaa ogdmaaaaaaaaaa"
+ * 
+ * 
+ * 
+ * 
+ * Notes:
+ * 
+ * 
+ * S contains only uppercase, lowercase and spaces. Exactly one space between
+ * each word.
+ * 1 <= S.length <= 150.
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    string toGoatLatin(string S) {
+        unordered_set<char> vowels({'a', 'e', 'i', 'o', 'u', 'A'});
+        istringstream iss(S);
+        string result = "", word = "";
+        int idx = 0;
+        while (iss >> word) {
+            ++idx;
+            if (vowels.count(tolower(word[0])) == 0) {
+                word += word[0];
+                word.erase(begin(word));
+            }
+            result += word + "ma";
+            result.append(idx, 'a');
+            result += " ";
+        }
+        if (result.back() == ' ')
+            result.pop_back();
+        return result;
+    }
+};
+// @lc code=end
