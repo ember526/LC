70649647708ef52f101c40f682dab80a4339e563
RingInFour <15347109301@163.com>
2019-10-31 23:25:16 -0500

    update 917.reverse-only-letters.cpp

---
+++ 917.reverse-only-letters.cpp
@@ -0,0 +1,85 @@
+/*
+ * @lc app=leetcode id=917 lang=cpp
+ *
+ * [917] Reverse Only Letters
+ *
+ * https://leetcode.com/problems/reverse-only-letters/description/
+ *
+ * algorithms
+ * Easy (56.20%)
+ * Likes:    328
+ * Dislikes: 30
+ * Total Accepted:    38.3K
+ * Total Submissions: 67.8K
+ * Testcase Example:  '"ab-cd"'
+ *
+ * Given a string S, return the "reversed" string where all characters that are
+ * not a letter stay in the same place, and all letters reverse their
+ * positions.
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: "ab-cd"
+ * Output: "dc-ba"
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: "a-bC-dEf-ghIj"
+ * Output: "j-Ih-gfE-dCba"
+ * 
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: "Test1ng-Leet=code-Q!"
+ * Output: "Qedo1ct-eeLg=ntse-T!"
+ * 
+ * 
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * S.length <= 100
+ * 33 <= S[i].ASCIIcode <= 122 
+ * S doesn't contain \ or "
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    string reverseOnlyLetters(string S) {
+        int l = 0, r = S.length() - 1;
+        while (l < r) {
+            while (l < r && !isalpha(S[l])) ++l; 
+            while (l < r && !isalpha(S[r])) --r; 
+            if (l < r)
+                swap(S[l++], S[r--]);
+        }
+        return S;
+    }
+};
+// @lc code=end
